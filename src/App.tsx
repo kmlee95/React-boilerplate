db929e9818d2bed3eb89998d7834b5bbe71f6fdf
kmlee95 <kmlee95@naver.com>
2021-07-18 17:31:58 +0900

    chore : project init setting

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Home from '@/pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
